36356953276a1fdf47c87c65c485de93a75c6acb
EvangelistaL <lucas.silva@sptech.school>
2024-04-24 17:54:58 -0300

    first commit

---
+++ webhook-receiver/src/test/java/com/casealpe/webhookreceiver/WebhookReceiverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.casealpe.webhookreceiver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebhookReceiverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
