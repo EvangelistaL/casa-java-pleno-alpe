9c5a21943ae9b8b6b7b4e148bc25498fc26ee41d
EvangelistaL <lucas.silva@sptech.school>
2024-05-06 18:59:45 -0300

    adding new project: nfe-api

---
+++ nfe-api/src/main/java/com/casealpe/nfeapi/NfeApiApplication.java
@@ -0,0 +1,13 @@
+package com.casealpe.nfeapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NfeApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NfeApiApplication.class, args);
+	}
+
+}
---
+++ nfe-api/src/test/java/com/casealpe/nfeapi/NfeApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.casealpe.nfeapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NfeApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
